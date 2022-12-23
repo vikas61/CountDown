54d6cef6c1fbcd0d14e64de34647c433165804af
vikas61 <81066106+vikas61@users.noreply.github.com>
2022-12-23 06:49:41 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const endDate="20 January 10:00 AM"
+const endDate="20 January 2022 10:00 AM"
 
 document.getElementById("end-date").innerText=endDate;
 const inputs = document.querySelectorAll("input")
